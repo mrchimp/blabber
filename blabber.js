44ed26b04016719af511d1833ca1c688b0a08358
mrchimp <chimpytk@gmail.com>
2013-03-04 03:36:38 +0000

    consolified, added pms, broke usernames

--- blabber.js
+++ blabber.js
@@ -2,7 +2,9 @@ var express = require('express'),
     app = express(),
     http = require('http'),
     server = http.createServer(app),
-    io = require('socket.io').listen(server);
+    io = require('socket.io').listen(server),
+    check = require('validator').check,
+    sanitize = require('validator').sanitize;
 
 server.listen(8080, "0.0.0.0");
 
@@ -10,24 +12,32 @@ server.listen(8080, "0.0.0.0");
 app.use(express.static(__dirname + '/www'));
 
 // usernames which are currently connected to the chat
-var usernames = {};
+var usernames = {},
+    socket_map = {};
 
 io.sockets.on('connection', function (socket) {
 
     // when the client emits 'sendchat', this listens and executes
     socket.on('sendchat', function (data) {
+        // sanitize user input
+        data = sanitize(data).xss();
+    
         // we tell the client to execute 'updatechat' with 2 parameters
         io.sockets.emit('updatechat', socket.username, data);
     });
 
     // when the client emits 'adduser', this listens and executes
-    socket.on('adduser', function(username){
+    socket.on('adduser', function (username) {
+        // sanitize username
+        username = sanitize(username).xss();
+        // map username to socket id
+        socket_map[username] = socket.id;
         // we store the username in the socket session for this client
         socket.username = username;
         // add the client's username to the global list
         usernames[username] = username;
         // echo to client they've connected
-        socket.emit('updatechat', 'SERVER', 'Greetings!');
+        socket.emit('updatechat', 'SERVER', 'Greetings! ' + usernames.length + ' users online.');
         // echo globally (all clients) that a person has connected
         socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
         // update the list of users in chat, client-side
@@ -35,12 +45,21 @@ io.sockets.on('connection', function (socket) {
     });
     
     // when the user disconnects.. perform this
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         // remove the username from global usernames list
         delete usernames[socket.username];
         // update list of users in chat, client-side
         io.sockets.emit('updateusers', usernames);
         // echo globally that this client has left
         socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+    });
+    
+    socket.on('getuserlist', function () {
+        socket.emit('refresh_usernames', usernames);
+    });
+    
+    socket.on('pm', function (username, message) {
+        var recipient = socket_map[username];
+        io.sockets.socket(recipient).emit('updatechat', 'PM', message);
     });
 });
--- www/js/chat.js
+++ www/js/chat.js
@@ -10,8 +10,8 @@ function zeroPad(str, len) {
 var Blabber = function () {
     this.window_title = document.title;
     this.disconnected = true;
-    self.alert_timeout;
-
+    this.alert_timeout;
+    this.username = '';
     // set up audio
     this.$new_msg_sound = document.createElement('audio');
     this.audioTagSupport = !!(this.$new_msg_sound.canPlayType);
@@ -38,13 +38,22 @@ var Blabber = function () {
                             });
         $.get();
     }
+    
+    this.focusOnInput();
 };
 
 Blabber.prototype.connect = function () {
     var t = this;
     
+    if (this.disconnect === false) {
+        return true;
+    }
+    
     this.disconnected = false;
+    this.users = '';
+    this.appendMessage('CONNECTING', 'Please be patient...');
     this.socket = io.connect('http://' + window.location.hostname + ':8080');
+    this.my_username = '';
     
     // listener, whenever the server emits 'updatechat', this updates the chat body
     this.socket.on('updatechat', function (username, data) {
@@ -65,26 +74,34 @@ Blabber.prototype.connect = function () {
 
     // on connection to server, ask for user's name with an anonymous callback    
     this.socket.on('connect', function () {
-        t.current_username = prompt("What's your name?");
+        //t.current_username = prompt("What's your name?");
 
         // call the server-side function 'adduser' and send one parameter (value of prompt)
-        t.socket.emit('adduser', t.current_username);
-        $('#conversation').html('');
-        $('#connect').removeClass('disconnected').addClass('connected');
+        console.log(t);
+        console.log('adding user: ' + t.my_username);
+        t.socket.emit('adduser', t.my_username);
+        t.clear();
+        $('#prompt').removeClass('disconnected').addClass('connected');
     });
 
     // listener, whenever the server emits 'updateusers', this updates the username list
     this.socket.on('updateusers', function (data) {
         $('#users').empty();
+        t.users = data;
         $.each(data, function (key, value) {
-            $('#users').append('<li><a href="#">' + key + '</a></li>');
+            $('#users').append('<li>' + key + '</li>');
         });
     });
     
     this.socket.on('disconnect', function () {
-        this.disconnected = true;
-        $('#connect').removeClass('connected').addClass('disconnected');
+        t.disconnected = true;
+        $('#prompt').removeClass('connected').addClass('disconnected');
     });
+};
+
+Blabber.prototype.setUserName = function (username) {
+    console.log(setting username to: ' + username);
+    this.username = username;
 };
 
 Blabber.prototype.disconnect = function () {
@@ -97,37 +114,105 @@ Blabber.prototype.resetTitle = function () {
     document.title = self.window_title;
 };
 
-Blabber.prototype.appendMessage = function (username, message) {        
+Blabber.prototype.clear = function () {
+    $('msg_out').html('');
+}
+
+Blabber.prototype.appendMessage = function (username, message) {
+    console.log(username);
     var currentdate = new Date(),
         datetime = currentdate.getFullYear() + "/"
                  + zeroPad(currentdate.getMonth() + 1, 2) + "/"
                  + zeroPad(currentdate.getDate(), 2) + " "
                  + zeroPad(currentdate.getHours(), 2) + ":"
                  + zeroPad(currentdate.getMinutes(), 2) + ":"
-                 + zeroPad(currentdate.getSeconds(), 2);
-
-    $('#conversation').append('<span title="Message sent ' + datetime + '"><b>' + username + ':</b> ' + message + '</span><br>');
+                 + zeroPad(currentdate.getSeconds(), 2),
+        isme = '';
+        
+    if (username === this.my_username) {
+        isme = ' me';
+    }
+    
+    $('#msg_out').append('<span title="Message sent ' + datetime + '"><span class="username' + isme + '">' + username + ':</span> ' + message + '</span><br>');
     $('body').scrollTop($("body")[0].scrollHeight);
 };
 
 Blabber.prototype.sendMessage = function (message) {
     if (this.disconnected === true) {
-        this.appendMessage('ERROR', 'Cant send message - disconnected.<br>');
+        this.appendMessage('ERROR', 'Cant send message - disconnected.');
+        this.focusOnInput();
         return false;
     }
 
     // tell server to execute 'sendchat' and send along one parameter
     this.socket.emit('sendchat', message);
+    this.focusOnInput();
+};
+
+Blabber.prototype.sendPM = function (username, message) {
+    this.socket.emit('pm', username, message);
+}
+
+Blabber.prototype.focusOnInput = function() {
+  var target_top = $('#page_bottom').offset().top;
+
+  $('html, body').animate({
+    duration: 'fast',
+    scrollTop: target_top}, 10);
+    
+  $('#msg_in').focus();
 };
 
 $(document).ready(function () {
     var blab = new Blabber();
 
-    // when the client hits ENTER on their keyboard
-    $('#msg_input').keypress(function (e) {
-        if (e.which === 13) {
-            blab.sendMessage($('#msg_input').val());
-            $('#msg_input').val('');
+    // Always keep focus on input
+    $('body, html').click(blab.focusOnInput);
+    $(window).resize(blab.focusOnInput);
+    
+    $('#msg_in').keypress(function (e) {
+        if (e.which === 13) { // Enter
+            var msg = $('#msg_in').val();
+            var msg_split = msg.split(' ', 2);
+            
+            if (msg.charAt(0) === '@') {
+                blab.sendPM(msg_split[0].substr(1), msg_split[1]);
+                return true;
+            }
+            
+            switch (msg_split[0]) {
+                case '/connect':
+                    if (msg_split.length < 2) {
+                        blab.appendMessage('HINT', 'Enter your username after /connect.');
+                        break;
+                    }
+                    blab.setUserName(msg_split[1]);
+                    blab.connect();
+                    $('#msg_in').val('');
+                    break;
+                case '/disconnect':
+                    blab.disconnect();
+                    $('#msg_in').val('');
+                    break;
+                case '/users':
+                    $('#userlist').toggle();
+                    $('body').toggleClass('sidebar');
+                    $('#msg_in').val('');
+                    break;
+                case '/help':
+                    blab.appendMessage('HELP', 'Available commands:<br>'
+                        + '/connect - connect<br>'
+                        + '/disconnect - disconnect<br>'
+                        + '/users - toggle user list<br>'
+                        + '/help - you just did that<br>'
+                        + 'Send a pm by prefixing @<em>username</em>'
+                    );
+                    $('#msg_in').val('');
+                    break;
+                default:
+                    blab.sendMessage(msg);
+                    $('#msg_in').val('');
+            }
         }
     });
 
@@ -135,7 +220,6 @@ $(document).ready(function () {
         e.preventDefault();
         
         if (blab.disconnected === true) {
-            blab.appendMessage('CONNECTING', 'Please be patient...<br>');
             blab.connect();
         } else {
             blab.disconnect();
