6348fd909e8f84845832331c56061283337661ab
MrChimp <chimpytk@gmail.com>
2013-11-23 02:25:24 +0000

    updated bootstrap

--- blabber.js
+++ blabber.js
@@ -3,63 +3,49 @@ var express = require('express'),
     http = require('http'),
     server = http.createServer(app),
     io = require('socket.io').listen(server),
-    check = require('validator').check,
-    sanitize = require('validator').sanitize;
+    ent = require('ent');
 
-server.listen(8080, "0.0.0.0");
+
+server.listen(80, "0.0.0.0");
 
 // routing
 app.use(express.static(__dirname + '/www'));
 
 // usernames which are currently connected to the chat
-var usernames = {},
-    socket_map = {};
+var usernames = {};
 
 io.sockets.on('connection', function (socket) {
 
     // when the client emits 'sendchat', this listens and executes
     socket.on('sendchat', function (data) {
-        // sanitize user input
-        data = sanitize(data).xss();
-    
         // we tell the client to execute 'updatechat' with 2 parameters
-        io.sockets.emit('updatechat', socket.username, data);
+        io.sockets.emit('updatechat', ent.encode(socket.username), ent.encode(data));
     });
 
     // when the client emits 'adduser', this listens and executes
-    socket.on('adduser', function (username) {
-        // sanitize username
-        username = sanitize(username).xss();
-        // map username to socket id
-        socket_map[username] = socket.id;
+    socket.on('adduser', function(username){
+        var safe_name = ent.encode(username);
         // we store the username in the socket session for this client
-        socket.username = username;
+        socket.username = safe_name;
         // add the client's username to the global list
-        usernames[username] = username;
+        usernames[username] = safe_name;
         // echo to client they've connected
-        socket.emit('updatechat', 'SERVER', 'Greetings! ' + usernames.length + ' users online.');
+        socket.emit('updatechat', 'SERVER', 'Greetings!');
         // echo globally (all clients) that a person has connected
-        socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+        socket.broadcast.emit('updatechat', 'SERVER', safe_name + ' has connected');
         // update the list of users in chat, client-side
         io.sockets.emit('updateusers', usernames);
     });
     
     // when the user disconnects.. perform this
-    socket.on('disconnect', function () {
+    socket.on('disconnect', function(){
+        var safe_name = ent.encode(socket.username);
+
         // remove the username from global usernames list
-        delete usernames[socket.username];
+        delete usernames[safe_name];
         // update list of users in chat, client-side
         io.sockets.emit('updateusers', usernames);
         // echo globally that this client has left
-        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-    });
-    
-    socket.on('getuserlist', function () {
-        socket.emit('refresh_usernames', usernames);
-    });
-    
-    socket.on('pm', function (username, message) {
-        var recipient = socket_map[username];
-        io.sockets.socket(recipient).emit('updatechat', 'PM', message);
+        socket.broadcast.emit('updatechat', 'SERVER', safe_name + ' has disconnected');
     });
 });
